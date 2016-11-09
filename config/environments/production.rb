eac931ef946ff2d65240d5145afccc292c39dc50
dogukan1905 <dogukanocak05@gmail.com>
2016-11-10 00:38:26 +0200

    for ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,8 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
+  config.ssl_options = { hsts: false }
+
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
